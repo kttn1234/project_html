f2f201dd3a36a4857f91694b21fdaa9697234111
xuanttd <kttn1234x@gmail.com>
2024-08-01 08:35:44 +0700

    add pagination and hs80

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -32,11 +32,12 @@ $(document).ready(function () {
 
   $dropdown.click(function (e) {
     e.stopPropagation();
+    $(this).parents(".dropdown").toggleClass(active);
     $(this).toggleClass(active);
   });
 
   $dropdownItem.click(function () {
-    $dropdownName.html($(this).text());
+    $(this).parents(".dropdown").find(".dropdown__name").html($(this).text());
     $dropdownItem.removeClass(isSelect);
     $(this).addClass(isSelect);
   });
@@ -133,8 +134,6 @@ $(document).ready(function () {
 
   // $(".has-dropdown").addClass("actitive")
 
-
-
   $('.has-dropdown').next().toggle();
   $('.has-dropdown').toggleClass("active")
 
@@ -171,6 +170,23 @@ $(document).ready(function () {
         $this.attr('title', $this.text());
     }
   });
+  // com js show text full
+
+  $('.profile__txt').bind('mouseenter', function(){
+    var $this = $(this);
+
+    if(this.offsetWidth < this.scrollWidth && !$this.attr('title')){
+        $this.attr('title', $this.text());
+    }
+  });
+
+  $('.js-hover-full').bind('mouseenter', function(){
+    var $this = $(this);
+
+    if(this.offsetWidth < this.scrollWidth && !$this.attr('title')){
+        $this.attr('title', $this.text());
+    }
+  });
 
   // accordion
   // $(".accordion-main__head").removeClass("active")
@@ -186,6 +202,10 @@ $(document).ready(function () {
     var heightFuntion = $(".profile__funtion").innerHeight();
     var heightTblHead = $(".tbl-profile__head").innerHeight();
     var heightPopupHead = $(".popup-detail__head").innerHeight();
+
+   
+    
+
     var heightScrollBar = 18;
     
     var heightTblBody = heigthScreen - heightHeadApp - heightMenuApp - heightFuntion - heightTblHead - heightScrollBar;
@@ -197,6 +217,15 @@ $(document).ready(function () {
     $(".profile__nav").css("height", heightProfileNav+"px");
 
     $(".popup-detail__body").css("height", heightPopupBody+"px");
+    if ($("body").hasClass("no-scroll")) {
+       // tagrget page
+        var heightTargerHead = $(".targer-main__head").innerHeight();
+        var heightTargerNav = $(".targer-nav.js-targer-nav").outerHeight(true);
+        // var heightPaddingTargerBody = parseInt( $(".targer-main__body").css("padding").substring(-2,2))
+
+        var heightTblBody = heigthScreen - heightHeadApp - heightMenuApp - heightTargerHead - heightTargerNav - heightFuntion - heightTblHead - heightScrollBar;
+        $(".tbl-profile__body").css("height", heightTblBody+"px");
+    }
   }
 
   $( window ).on( "resize", function() {
@@ -236,5 +265,354 @@ $(document).ready(function () {
 
   // add class to hover
   $(".tooltip-menu").parents(".profile__link").addClass("has-more")
+
+  // chartjs
+  const Utils = ChartUtils.init()
+
+  var NUM_DATA = 130;
+  var NUM_CFG = {count: NUM_DATA, min: 0, max: 0.2};
+
+  var dataMonthly = {
+    // labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+    labels: ["Jul 2024"],
+    // labels: Utils.months({count: NUM_DATA}),
+    datasets: [
+      {
+        label: "Postings",
+        backgroundColor: "#de314a",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        // data: Utils.numbers(NUM_CFG),
+      },
+      {
+        label: "Sent Comments",
+        backgroundColor: "#f78c5a",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        // data: Utils.numbers(NUM_CFG),
+      },
+      {
+        label: "Chat Messages",
+        backgroundColor: "#ffe76b",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        // data: Utils.numbers(NUM_CFG),
+      },
+      {
+        label: "Media Items",
+        backgroundColor: "#319cce",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        // data: Utils.numbers(NUM_CFG),
+      },
+    ]
+  };
+
+  var dataYearly = {
+    // labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+    labels: ["2024"],
+    // labels: Utils.months({count: NUM_DATA}),
+    datasets: [
+      {
+        label: "Postings",
+        backgroundColor: "#de314a",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        // data: Utils.numbers(NUM_CFG),
+      },
+      {
+        label: "Sent Comments",
+        backgroundColor: "#f78c5a",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        // data: Utils.numbers(NUM_CFG),
+      },
+      {
+        label: "Chat Messages",
+        backgroundColor: "#ffe76b",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        // data: Utils.numbers(NUM_CFG),
+      },
+      {
+        label: "Media Items",
+        backgroundColor: "#319cce",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        // data: Utils.numbers(NUM_CFG),
+      },
+    ]
+  };
+
+  var dataDaily = {
+    // labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+    labels: ["Jul 2024"],
+    labels: ["Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday", "Sunday"],
+    datasets: [
+      {
+        label: "Postings",
+        backgroundColor: "#de314a",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        // data: Utils.numbers(NUM_CFG),
+      },
+      {
+        label: "Sent Comments",
+        backgroundColor: "#f78c5a",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        // data: Utils.numbers(NUM_CFG),
+      },
+      {
+        label: "Chat Messages",
+        backgroundColor: "#ffe76b",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        // data: Utils.numbers(NUM_CFG),
+      },
+      {
+        label: "Media Items",
+        backgroundColor: "#319cce",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        // data: Utils.numbers(NUM_CFG),
+      },
+    ]
+  };
+
+  var NUM_DATA_HOURS = 130;
+  var NUM_CFG_HOURS = {count: NUM_DATA, min: 0, max: 0.2};
+  var dataHourly = {
+    // labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+    // labels: ["Jul 2024"],
+    labels: ["00 - 01", "01 - 02", "02 - 03", "03 - 04", "05 - 06", "06 - 07", "07 - 08", "08 - 09", "10 - 11", "11 - 12", "12 - 13", "13 - 14", "14 - 15", "15 - 16", "16 - 17", "17 - 18", "18 - 19", "20 - 21", "21 - 22", "22 - 23", "23 - 24"],
+    datasets: [
+      {
+        label: "Postings",
+        backgroundColor: "#de314a",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: Utils.numbers(NUM_CFG_HOURS),
+      },
+      {
+        label: "Sent Comments",
+        backgroundColor: "#f78c5a",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: Utils.numbers(NUM_CFG_HOURS),
+      },
+      {
+        label: "Chat Messages",
+        backgroundColor: "#ffe76b",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: Utils.numbers(NUM_CFG_HOURS),
+      },
+      {
+        label: "Media Items",
+        backgroundColor: "#319cce",
+        borderColor: "#ffffff",
+        borderWidth: 0,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: Utils.numbers(NUM_CFG_HOURS),
+      },
+    ]
+  };
+  
+  var optionsMonthly = {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        stacked: true,
+        grid: {
+          display: true,
+          color: "#f7f7f7"
+        },
+       
+      },
+      x: {
+        grid: {
+          display: false
+        }
+      },
+      
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: 'Monthly',
+        align: 'start'
+      }
+     
+    }
+  };
+
+
+  var optionsYearly = {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        stacked: true,
+        grid: {
+          display: true,
+          color: "#f7f7f7"
+        }
+      },
+      x: {
+        grid: {
+          display: false
+        }
+      }
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: 'Yearly',
+        align: 'start'
+      }
+     
+    }
+  };
+
+  var optionsDaily = {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        stacked: true,
+        grid: {
+          display: true,
+          color: "#f7f7f7"
+        }
+      },
+      x: {
+        grid: {
+          display: true
+        }
+      }
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: 'Daily',
+        align: 'start'
+      }
+     
+    }
+  };
+
+  var optionsHourly = {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        stacked: true,
+        grid: {
+          display: true,
+          color: "#f7f7f7"
+        }
+      },
+      x: {
+        grid: {
+          display: true
+        }
+      }
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: 'Hourly',
+        align: 'start'
+      }
+     
+    }
+  };
+  
+ 
+  if($(".chart-date").length) {
+    new Chart('chartMonthly', {
+      type: 'bar',
+      options: optionsMonthly,
+      data: dataMonthly
+    });
+    new Chart('chartYearly', {
+      type: 'bar',
+      options: optionsYearly,
+      data: dataYearly
+    });
+
+    new Chart('chartDaily', {
+      type: 'bar',
+      options: optionsDaily,
+      data: dataDaily
+    });
+
+    new Chart('chartHourly', {
+      type: 'bar',
+      options: optionsHourly,
+      data: dataHourly
+    });
+  }
+
+  
+
+  // tab
+  $(".js-targer-nav .targer-nav__item").not(".--dropdown").click(function() {
+    $('.js-targer-nav .targer-nav__item').removeClass('active');
+   
+    $(this).addClass('active');
+    $('.targer-content__item').hide();
+    
+    // var activeTab = $(this).attr('data-tab');
+    // $("#"+activeTab).fadeIn();
+    var index = $(this).index();
+    $(".targer-content__item").eq(index).fadeIn()
+    $("body").removeClass("no-scroll")
+  });
+
+  $(".js-targer-nav .dropdown__item").click(function(e) {
+    e.preventDefault()
+      $('.js-targer-nav .targer-nav__item').removeClass('active');
+      $('.js-targer-nav .targer-nav__item.--dropdown').addClass("active");
+      $('.targer-content__item').hide();
+      $(".targer-content__item#tab-dropdown").fadeIn();
+      $(this).parents(".dropdown").toggleClass("--active")
+      $("body").addClass("no-scroll")
+
+      setheightScroll();
+  })
+  
+  $(".js-targer-nav .dropdown__list").click(function(e) {
+    e.stopPropagation();
+  })
 
 });
